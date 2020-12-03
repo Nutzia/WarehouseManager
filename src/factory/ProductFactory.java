76abffe1c46e90f549c51c6e557f932c764e2ea0
Nutzia VAN DEN EERDEWEGH <r0262372@ucll.be>
2020-12-03 22:28:41 +0100

    Create factories for Product and Stock

--- src/factory/ProductFactory.java
+++ src/factory/ProductFactory.java
@@ -1,8 +1,10 @@
 package factory;
 
 import java.util.Random;
-import product.Product;
-import product.ProductType;
+
+import product.*;
+import stock.Stock;
+import stock.StockDirection;
 
 public class ProductFactory {
 
@@ -10,24 +12,62 @@ public class ProductFactory {
 
     private static Product product;
 
+    private static int[] getCapacities() {
+        int[] randomCapacities = new int[3];
+
+        int min = random.nextInt(10) + 1;
+        int max = min + random.nextInt(20) + 1;
+        int crit = Math.round(min / 2);
+
+        randomCapacities[0] = min;
+        randomCapacities[1] = max;
+        randomCapacities[2] = crit;
+
+        return randomCapacities;
+    }
+
     public static Product getProduct(ProductType productType){
-        //product = new Product();
+        int[] capacities = getCapacities();
+
         switch (productType) {
             case MONSTERA:
-                //product = new Product();
+                product = new Monstera(capacities[0], capacities[1], capacities[2]);
                 break;
             case PANCAKEPLANT:
-                //product = new Product();
+                product = new PancakePlant(capacities[0], capacities[1], capacities[2]);
                 break;
             case SANSEVERIA:
-                //product = new Product();
+                product = new Sanseveria(capacities[0], capacities[1], capacities[2]);
                 break;
             case PEACELILY:
-                //product = new Product();
+                product = new PeaceLily(capacities[0], capacities[1], capacities[2]);
                 break;
             default:
                 return null;
         }
-    return product;
+
+        int numberMovements = 1 + random.nextInt(10);
+        StockDirection[] stockMovements = StockDirection.values();
+
+        for (int i = 0; i < numberMovements; i++) {
+            StockDirection randomDirection;
+
+            if (i == 0) {
+                randomDirection = StockDirection.INITIAL;
+            } else {
+                randomDirection = stockMovements[random.nextInt(stockMovements.length)];
+            }
+
+            Stock stock = StockFactory.getStock(randomDirection);
+
+            if (i == 0) {
+                int randomInitialStockAmount = random.nextInt(product.maxStock - product.minStock) + product.minStock;
+                stock.setAmount(randomInitialStockAmount);
+            }
+
+            product.doStockMovement(stock);
+        }
+
+        return product;
     }
 }
--- src/factory/StockFactory.java
+++ src/factory/StockFactory.java
@@ -1,8 +1,43 @@
 package factory;
 
+import stock.Stock;
+import stock.StockDirection;
+
 import java.util.Random;
 
-public class StockmovementsFactory {
+public class StockFactory {
 
     public static final Random random = new Random();
+
+    private static Stock stock;
+
+    public static Stock getStock(StockDirection stockDirection) {
+      stock = new Stock(stockDirection);
+
+      switch (stockDirection) {
+          case INITIAL:
+              // Initial stock amount decided by ProductFactory due to amount limitations unknown by StockFactory
+              break;
+          case BUY:
+              stock.setAmount(random.nextInt(5));
+              break;
+          case SELL:
+              stock.setAmount(random.nextInt(4));
+              break;
+          case TRANSPORT:
+              stock.setAmount(random.nextInt(3));
+              break;
+          case PRODUCTION:
+              stock.setAmount(random.nextInt(9));
+              break;
+          case QUARANTAINE:
+              stock.setAmount(random.nextInt(3));
+              break;
+          case RESERVATION:
+              stock.setAmount(random.nextInt(7));
+              break;
+      }
+
+      return stock;
+    }
 }
